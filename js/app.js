238eaca9e9d86a279b2c7fdbd2783dfa2ce0a14a
mateuszrosa <mateusz.rosa@gmail.com>
2019-12-01 21:16:37 +0100

    show class setting height 100% of ul

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const bar = document.querySelector("i");
-const ul = document.querySelector("ul");
+const ul = document.querySelector("nav.mobile ul");
 bar.addEventListener("click", () => {
   ul.classList.add("show");
   console.log("object");
