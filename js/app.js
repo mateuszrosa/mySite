8b70c1c5d4a05ef9edb515685338585bbb0023ab
mateuszrosa <mateusz.rosa@gmail.com>
2019-12-03 00:48:40 +0100

    styling menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const bar = document.querySelector(".icon");
-const ul = document.querySelector("nav.mobile ul");
+const ul = document.querySelector("nav.mobile");
 bar.addEventListener("click", () => {
   ul.classList.toggle("show");
 });
