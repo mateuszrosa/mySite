8a27d81f55bc406f5248b1455a222811ca48712c
mateuszrosa <mateusz.rosa@gmail.com>
2019-12-02 17:48:18 +0100

    new vision of mobile menu

--- js/app.js
+++ js/app.js
@@ -2,5 +2,4 @@ const bar = document.querySelector("i");
 const ul = document.querySelector("nav.mobile ul");
 bar.addEventListener("click", () => {
   ul.classList.toggle("show");
-  console.log("object");
 });
