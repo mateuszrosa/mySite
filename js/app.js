ac8efd1ecc9499aaa3f1b221e04908dd9ea60194
mateuszrosa <mateusz.rosa@gmail.com>
2019-12-02 23:46:39 +0100

    mobile menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const bar = document.querySelector("i");
+const bar = document.querySelector(".icon");
 const ul = document.querySelector("nav.mobile ul");
 bar.addEventListener("click", () => {
   ul.classList.toggle("show");
