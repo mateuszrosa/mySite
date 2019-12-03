a0faae9f1e60f5b18030a469c22beb4f9cdb6dca
mateuszrosa <mateusz.rosa@gmail.com>
2019-12-03 11:45:38 +0100

    icon.active

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const bar = document.querySelector(".icon");
 const ul = document.querySelector("nav.mobile");
+const icon = document.querySelector(".icon");
 bar.addEventListener("click", () => {
   ul.classList.toggle("show");
+  icon.classList.toggle("active");
 });
