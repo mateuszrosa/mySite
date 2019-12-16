f32b557bebb68640d515863679067d00171a2dcd
mateuszrosa <mateusz.rosa@gmail.com>
2019-12-16 23:02:59 +0100

    adding white background to .icon after scroll down more or equal pixels as header height

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 const bar = document.querySelector(".icon");
 const ul = document.querySelector("nav.mobile");
 const icon = document.querySelector(".icon");
+const header = document.querySelector("header");
+let flag;
+
 bar.addEventListener("click", () => {
   ul.classList.toggle("show");
   icon.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= header.clientHeight && flag) {
+    console.log("object");
+    icon.style.background = "white";
+    flag = false;
+    return;
+  } else if (window.pageYOffset <= header.clientHeight && !flag) {
+    icon.style.background = "none";
+    flag = true;
+  }
 });
