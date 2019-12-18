5477190dbc92bf71e28fbafbee949b01d4c63660
mateuszrosa <mateusz.rosa@gmail.com>
2019-12-18 17:10:50 +0100

    changing color of hamburger

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
 const bar = document.querySelector(".icon");
 const ul = document.querySelector("nav.mobile");
 const icon = document.querySelector(".icon");
+const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector("header");
 let flag;
 
 bar.addEventListener("click", () => {
   ul.classList.toggle("show");
   icon.classList.toggle("active");
+  if (ul.classList.contains("show")) {
+    hamburger.classList.remove("test");
+  } else {
+    hamburger.classList.add("test");
+  }
 });
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset >= header.clientHeight && flag) {
-    icon.querySelector(".hamburger").classList.add("test");
+  if (
+    window.pageYOffset >= header.clientHeight &&
+    flag &&
+    !ul.classList.contains("show")
+  ) {
+    hamburger.classList.add("test");
     flag = false;
     return;
   } else if (window.pageYOffset <= header.clientHeight && !flag) {
-    icon.querySelector(".hamburger").classList.remove("test");
+    hamburger.classList.remove("test");
     flag = true;
   }
 });
