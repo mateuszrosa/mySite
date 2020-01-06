7c1d265be1cb8e5aa5561c54d82e69c65e5e90b1
mateuszrosa <mateusz.rosa@gmail.com>
2020-01-06 22:08:00 +0100

    loader wrapper

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@ const icon = document.querySelector(".icon");
 const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector("header");
 let flag;
+const loader = document.querySelector("#loader-wrapper");
+
+setTimeout(() => {
+  loader.style.display = "none";
+}, 2000);
 
 if (window.pageYOffset >= header.clientHeight) {
   hamburger.classList.add("test");
