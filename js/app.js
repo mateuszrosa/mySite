89b79e3f4f31c36132d96e20c26cab5db48567e4
mateuszrosa <mateusz.rosa@gmail.com>
2020-01-07 13:27:29 +0100

    animation on .about

--- js/app.js
+++ js/app.js
@@ -3,11 +3,22 @@ const ul = document.querySelector("nav.mobile");
 const icon = document.querySelector(".icon");
 const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector("header");
+const about = document.querySelector(".about");
 let flag;
 
 if (window.pageYOffset >= header.clientHeight) {
   hamburger.classList.add("test");
 }
+
+console.log(about.getBoundingClientRect().top);
+
+window.addEventListener("scroll", () => {
+  if (about.getBoundingClientRect().top <= 400) {
+    console.log(window.pageYOffset, about.getBoundingClientRect().top);
+    console.log("about");
+    about.querySelector(".container").classList.add("slide-right");
+  }
+});
 
 bar.addEventListener("click", () => {
   ul.classList.toggle("show");
